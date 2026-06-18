64986feeab47acc21220f8c1829f6a126b9cfce2
Naveen Sharma <naveenharsh2509@gmail.com>
2026-06-18 14:23:41 +0530

    wip

--- counselling/courses/views.py
+++ counselling/courses/views.py
@@ -1,28 +1,62 @@
+# from django.shortcuts import render
+# from courses.models import CoursesPage, ExamCategory, Course, TopCollege, NeedHelpSection
+# from common.models import Page
+
+
+# def courses(request):
+#     page = Page.objects.get(slug = 'courses')
+#     courses_page = CoursesPage.objects.select_related("guidance_section").prefetch_related("exam_categories__courses","exam_categories__top_colleges").first()
+    
+#     exam_category = request.GET.get("exam_category")
+#     courses = Course.objects.all()
+#     top_colleges = TopCollege.objects.all()
+
+#     # if exam_category:
+#     #     selected_category = courses_page.exam_categories.get(id = exam_category)
+             
+#     # else:
+#     #     selected_category = courses_page.exam_categories.first()
+        
+
+#     context = {
+#         "page" : page,
+#         "exam_category" : courses_page.exam_categories.all(),
+#         "courses" : courses,
+#         "top_colleges" : top_colleges,
+#         "guidance" : courses_page.guidance_section,   
+#     }
+#     return render(request, "courses.html" , context)
+
+
 from django.shortcuts import render
-from courses.models import CoursesPage, ExamCategory, Course, TopCollege, NeedHelpSection
+from courses.models import CoursesPage, Course, TopCollege
 from common.models import Page
 
 
 def courses(request):
-    page = Page.objects.get(slug = 'courses')
-    courses_page = CoursesPage.objects.select_related("guidance_section").prefetch_related("exam_categories__courses","exam_categories__top_colleges").first()
-    
+    page = Page.objects.get(slug='courses')
+
+    courses_page = CoursesPage.objects.prefetch_related(
+        "guidance_section",
+        "exam_categories__courses",
+        "exam_categories__top_colleges"
+    ).first()
+
     exam_category = request.GET.get("exam_category")
+
     courses = Course.objects.all()
     top_colleges = TopCollege.objects.all()
 
-    # if exam_category:
-    #     selected_category = courses_page.exam_categories.get(id = exam_category)
-             
-    # else:
-    #     selected_category = courses_page.exam_categories.first()
-        
+    # safety check (IMPORTANT)
+    exam_categories = courses_page.exam_categories.all() if courses_page else []
+    guidance = courses_page.guidance_section.all() if courses_page else []
 
     context = {
-        "page" : page,
-        "exam_category" : courses_page.exam_categories.all(),
-        "courses" : courses,
-        "top_colleges" : top_colleges,
-        "guidance" : courses_page.guidance_section,   
+        "page": page,
+        "exam_category": exam_categories,
+        "courses": courses,
+        "top_colleges": top_colleges,
+        "guidance": guidance,
     }
-    return render(request, "courses.html" , context)
+
+    return render(request, "courses.html", context)
